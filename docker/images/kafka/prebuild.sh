6cc9602fb43a6346500d3fbfc5ec40171d211016
Lachlan Orr <lorr@gaen.org>
2020-10-26 21:37:33 -0500

    kafka jmx -> prometheus, added postgres to compose

--- docker/images/kafka/prebuild.sh
+++ docker/images/kafka/prebuild.sh
@@ -5,5 +5,8 @@ KAFKA_VER=2.6.0
 export KAFKA_FILE=kafka_${SCALA_VER}-${KAFKA_VER}
 export KAFKA_TGZ=${KAFKA_FILE}.tgz
 
-echo "https://mirrors.sonic.net/apache/kafka/${KAFKA_VER}/${KAFKA_TGZ}"
 wget https://mirrors.sonic.net/apache/kafka/${KAFKA_VER}/${KAFKA_TGZ}
+
+JMX_AGENT_VER=0.14.0
+export JMX_AGENT_JAR=jmx_prometheus_javaagent-${JMX_AGENT_VER}.jar
+wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${JMX_AGENT_VER}/${JMX_AGENT_JAR}
--- docker/images/kafka/start.sh
+++ docker/images/kafka/start.sh
@@ -3,8 +3,18 @@
 cd /opt/kafka
 
 if [ "$1" = "zookeeper" ] ; then
+    export EXTRA_ARGS="-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Djava.util.logging.config.file=logging.properties \
+-javaagent:${JMX_AGENT_JAR}=9870:${JMX_AGENT_CONFIG_ZOOKEEPER}"
     bin/zookeeper-server-start.sh config/zookeeper.properties
 elif [ "$1" = "broker" ] ; then
+    export EXTRA_ARGS="-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Djava.util.logging.config.file=logging.properties \
+-javaagent:${JMX_AGENT_JAR}=9871:${JMX_AGENT_CONFIG_BROKER}"
     bin/kafka-server-start.sh config/server.properties
 else
     echo "usage: start.sh [zookeeper|broker]"
