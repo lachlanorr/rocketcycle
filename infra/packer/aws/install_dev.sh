3a488bdb007f4670a7564bb82ae25c136fd6b676
Lachlan Orr <lorr@gaen.org>
2021-09-04 09:17:22 -0500

    Normalized protoc versions, now no output differences

--- infra/packer/aws/install_dev.sh
+++ infra/packer/aws/install_dev.sh
@@ -1,5 +1,8 @@
 GO_VER=1.17
-FILE_NAME=go${GO_VER}.linux-amd64.tar.gz
+GO_FILE_NAME=go${GO_VER}.linux-amd64.tar.gz
+
+PROTOC_VER=3.17.3
+PROTOC_FILE_NAME=protoc-3.17.3-linux-x86_64.zip
 
 sudo apt update
 sudo apt update # needed, not sure why, but updates come down the second time
@@ -7,27 +10,37 @@ sudo apt upgrade
 
 sudo apt install -y make \
      build-essential \
-     protobuf-compiler \
+     unzip \
      emacs-nox
 
-sudo wget https://golang.org/dl/${FILE_NAME}
+sudo wget https://golang.org/dl/${GO_FILE_NAME}
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf ${FILE_NAME}
+sudo tar -C /usr/local -xzf ${GO_FILE_NAME}
 export PATH=$PATH:/usr/local/go/bin
 
 sudo mkdir /code
 sudo chown -R ubuntu:ubuntu /code
+mkdir /code/protobuf
+cd /code/protobuf
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/${PROTOC_FILE_NAME}
+unzip ${PROTOC_FILE_NAME}
+rm -f ${PROTOC_FILE_NAME}
+export PATH=${PATH}:/code/protobuf/bin
+
 cd /code
 git clone https://github.com/lachlanorr/rocketcycle.git
 
 cd rocketcycle
 go install \
-    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
-    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
-    google.golang.org/protobuf/cmd/protoc-gen-go \
-    google.golang.org/grpc/cmd/protoc-gen-go-grpc
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.0.1
+go install \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.0.1
+go install \
+    google.golang.org/protobuf/cmd/protoc-gen-go@v1.26.0
+go install \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.1
 
-echo 'PATH=$PATH:/usr/local/go/bin:~/go/bin' >> ~/.bashrc
+echo 'PATH=$PATH:/code/protobuf/bin:/usr/local/go/bin:~/go/bin' >> ~/.bashrc
 
 export PATH=$PATH:~/go/bin
 make
