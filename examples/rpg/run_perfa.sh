af22a8640fca83fa05a60acc0cfe1f7073a5c9b2
Lachlan Orr <lorr@gaen.org>
2021-10-07 15:12:37 -0500

    run_perfa.sh now uses perfa_env.sh to prep

--- examples/rpg/run_perfa.sh
+++ examples/rpg/run_perfa.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 ./init_db_perfa.sh
 
-DATABASE_URL=postgresql://postgres@postgresql-0.perfa.rkcy.net:5432/rpg
+. ./perfa_env.sh
 
 ./rpg platform update --config_file_path=platform_perfa.json --otelcol_endpoint=edge.perfa.rkcy.net:4317 --admin_brokers=kafka-0.clusa.perfa.rkcy.net:9093
 ./rpg run --otelcol_endpoint=edge.perfa.rkcy.net:4317 --admin_brokers=kafka-0.clusa.perfa.rkcy.net:9093
