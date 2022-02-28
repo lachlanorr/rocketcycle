70cf3aea36184e008fd8731621ff48e59b828055
Lachlan Orr <Lachlan.Orr@q2ebanking.com>
2022-02-28 10:12:57 -0600

    Updated docker kafka from 2.8.0 to 3.1.0

--- docker/images/kafka/prebuild.sh
+++ docker/images/kafka/prebuild.sh
@@ -5,12 +5,12 @@
 set -e
 
 SCALA_VER=2.13
-KAFKA_VER=2.8.0
+KAFKA_VER=3.1.0
 export KAFKA_FILE=kafka_${SCALA_VER}-${KAFKA_VER}
 export KAFKA_TGZ=${KAFKA_FILE}.tgz
 
-wget https://mirrors.sonic.net/apache/kafka/${KAFKA_VER}/${KAFKA_TGZ}
+wget https://dlcdn.apache.org/kafka/${KAFKA_VER}/${KAFKA_TGZ}
 
-JMX_AGENT_VER=0.14.0
+JMX_AGENT_VER=0.16.1
 export JMX_AGENT_JAR=jmx_prometheus_javaagent-${JMX_AGENT_VER}.jar
 wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/${JMX_AGENT_VER}/${JMX_AGENT_JAR}
